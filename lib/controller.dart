09ba69fb85a10b6d3a662cd8f17279f341789bd6
Gaurav <gauravswarankar1@gmail.com>
2021-05-16 20:05:50 +0530

    initial commit

---
+++ lib/controller.dart
@@ -0,0 +1,44 @@
+import 'dart:convert' as convert;
+import 'package:flutter_google_sheets/form.dart';
+import 'package:http/http.dart' as http;
+
+/// FormController is a class which does work of saving FeedbackForm in Google Sheets using
+/// HTTP GET request on Google App Script Web URL and parses response and sends result callback.
+class FormController {
+
+  // Google App Script Web URL.
+   static const String URL = "https://script.google.com/macros/s/AKfycbyAaNh-1JK5pSrUnJ34Scp3889mTMuFI86DkDp42EkWiSOOycE/exec";
+
+  var uri1 = Uri.parse(URL);
+  // Success Status Message
+  static const STATUS_SUCCESS = "SUCCESS";
+
+  /// Async function which saves feedback, parses [feedbackForm] parameters
+  /// and sends HTTP GET request on [URL]. On successful response, [callback] is called.
+  void submitForm(
+      FeedbackForm feedbackForm, void Function(String) callback) async {
+    try {
+      await http.post(uri1, body: feedbackForm.toJson()).then((response) async {
+        if (response.statusCode == 302) {
+          var url = response.headers['location'];
+          var uri2 = Uri.parse(url);
+          await http.get(uri2).then((response) {
+            callback(convert.jsonDecode(response.body)['status']);
+          });
+        } else {
+          callback(convert.jsonDecode(response.body)['status']);
+        }
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<List<FeedbackForm>> getFeedbackList() async {
+    return await http.get(uri1).then((response) {
+      var jsonFeedback = convert.jsonDecode(response.body) as List;
+      return jsonFeedback.map((json) => FeedbackForm.fromJson(json)).toList();
+    });
+  }
+
+}
